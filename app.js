833e52ef9eb1d9bad9c1790beed3788469552add
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-03-14 17:28:28 +0100

    working slider

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-const catPics = document.querySelectorAll('.pic--cat');
-const count = document.getElementById('count');
-var counter = 0;
+//const catPics = document.querySelectorAll('.pic--cat');
+//const count = document.getElementById('count');
+//var counter = 0;
+//
+//function clicksCounter() {
+////    console.log(this);
+//    counter++;
+//    count.textContent = counter;
+//}
+//
+//catPics.forEach(cat => cat.addEventListener('click', clicksCounter));
 
-function clicksCounter() {
-//    console.log(this);
-    counter++;
-    count.textContent = counter;
+
+// building slider
+
+const catNames = document.querySelectorAll('.cat__name');
+const catImgs = Array.from(document.querySelectorAll('.cat'));
+
+function showCat() {
+    catNames.forEach(function(e) {
+        e.classList.remove('cat__name--active');
+    });
+    catImgs.forEach(function(e) {
+        e.classList.remove('cat__pic--active');
+    });
+    this.classList.add('cat__name--active');
+    
+    for (var i=0; i<catImgs.length; i++) {
+        if (catImgs[i].dataset.cat === this.dataset.cat) {
+            catImgs[i].classList.add('cat__pic--active');
+        }
+    }
 }
 
-catPics.forEach(cat => cat.addEventListener('click', clicksCounter));
+catNames.forEach(catName => catName.addEventListener('click', showCat));
+
