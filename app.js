d690483497e25d8e5d957f5439dabf3c17865b02
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-03-19 19:06:22 +0100

    retype tutorial solution for better understanding

--- app.js
+++ app.js
@@ -1,90 +1,149 @@
-// model
+// *****============ solution from tutorial is more efficient ==== *****
+
+/* ======= Model ======= */
 
 const model = {
-    catName: "Soft(y)",
-    counter: 0,
-    nums: []
-}
-
-
-
-// octopus 
-
-const octopus = {
-    clicksCounter: function() {
-        const cats = document.querySelectorAll('.cat');
-        const arrCatPics = Array.from(document.querySelectorAll('.cat__pic'));
-        var inxOfThis = arrCatPics.indexOf(this);
-
-        if (!model.nums[inxOfThis]) {
-            model.nums[inxOfThis] = 0;
+    currentCat: null,
+    cats: [
+        {
+            clickCount: 0,
+            name: "Soft(y)",
+            imgSrc: "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426",
+            imgAttribution: "https://lh3.ggpht.com/nlI91wYNCrjjNy5f-S3CmVehIBM4cprx-JFWOztLk7vFlhYuFR6YnxcT446AvxYg4Ab7M1Fy0twaOCWYcUk=s0#w=640&h=426"
+        },
+        {
+            clickCount: 0,
+            name: "SpookY",
+            imgSrc: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496",
+            imgAttribution: "https://lh3.ggpht.com/kixazxoJ2ufl3ACj2I85Xsy-Rfog97BM75ZiLaX02KgeYramAEqlEHqPC3rKqdQj4C1VFnXXryadFs1J9A=s0#w=640&h=496"
+        },
+        {
+            clickCount: 0,
+            name: "Worms",
+            imgSrc: "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454",
+            imgAttribution: "https://lh5.ggpht.com/LfjkdmOKkGLvCt-VuRlWGjAjXqTBrPjRsokTNKBtCh8IFPRetGaXIpTQGE2e7ZCUaG2azKNkz38KkbM_emA=s0#w=640&h=454"
+        },
+        {
+            clickCount: 0,
+            name: "Shorty",
+            imgSrc: "http://media.jrn.com/images/0904_ksap_mckitten10904_23711927_ver1.0_640_480.jpg",
+            imgAttribution: "http://media.jrn.com/images/0904_ksap_mckitten10904_23711927_ver1.0_640_480.jpg"
+        },
+        {
+            clickCount: 0,
+            name: "Fluffy",
+            imgSrc: "https://s-media-cache-ak0.pinimg.com/564x/ce/b4/f2/ceb4f2baf424d7bb4c5c019bc4088786.jpg",
+            imgAttribution: "https://s-media-cache-ak0.pinimg.com/564x/ce/b4/f2/ceb4f2baf424d7bb4c5c019bc4088786.jpg"
         }
-        model.counter = model.nums[inxOfThis];
-        model.counter++;
-        model.nums[inxOfThis] = model.counter;
-
-        cats.forEach(function(cat) {
-            if (cat.classList.contains('cat__pic--active')) {
-                var catCountP = cat.querySelector('.count');
-                if(!catCountP) {
-                    catCountP = document.createElement('p');
-                    catCountP.classList.add('count');
-                    catCountP.textContent = model.counter;
-                    cat.append(catCountP);
-                } else {
-                    catCountP.textContent = model.counter;
-                }
-            }
-        });
-    }
-}
+    ]   
+};
 
 
-// view
+/* ======= Octopus ======= */
 
-const view = {
-    showName: function() {
-        const cats = document.querySelectorAll('.cat');
-        cats.forEach(function(cat) {
-            if (cat.classList.contains('cat__pic--active')) {
-                var catNameP = cat.querySelector('.cat--name');
-                if(!catNameP) {
-                    catNameP = document.createElement('p');
-                    catNameP.classList.add('cat--name');
-                    catNameP.textContent = catName;
-                    cat.prepend(catNameP);
-                } else {
-                    catNameP.textContent = catName;
-                }
-            }
-        });
+var octopus = {
+
+    init: function() {
+        // set our current cat to the first one in the list
+        model.currentCat = model.cats[0];
+
+        // tell our views to initialize
+        catListView.init();
+        catView.init();
     },
-    showCat: function() {
-        const catNames = document.querySelectorAll('.cat__name');
-        const catImgs = Array.from(document.querySelectorAll('.cat'));
-        catName = this.textContent;
-        catNames.forEach(function(e) {
-            e.classList.remove('cat__name--active');
-        });
-        catImgs.forEach(function(e) {
-            e.classList.remove('cat__pic--active');
-        });
-        this.classList.add('cat__name--active');
 
-        for (var i=0; i<catImgs.length; i++) {
-            if (catImgs[i].dataset.cat === this.dataset.cat) {
-                catImgs[i].classList.add('cat__pic--active');
-            }
+    getCurrentCat: function() {
+        return model.currentCat;
+    },
+
+    getCats: function() {
+        return model.cats;
+    },
+
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat) {
+        model.currentCat = cat;
+    },
+
+    // increments the counter for the currently-selected cat
+    incrementCounter: function() {
+        model.currentCat.clickCount++;
+        catView.render();
+    }
+};
+
+
+/* ======= View ======= */
+
+var catView = {
+
+    init: function() {
+        // store pointers to our DOM elements for easy access later
+        this.catElem = document.getElementById('cat');
+        this.catNameElem = document.getElementById('cat-name');
+        this.catImageElem = document.getElementById('cat-img');
+        this.countElem = document.getElementById('cat-count');
+
+        // on click, increment the current cat's counter
+        this.catImageElem.addEventListener('click', function(){
+            octopus.incrementCounter();
+        });
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        // update the DOM elements with values from the current cat
+        var currentCat = octopus.getCurrentCat();
+        this.countElem.textContent = currentCat.clickCount;
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.imgSrc;
+    }
+};
+
+var catListView = {
+
+    init: function() {
+        // store the DOM element for easy access later
+        this.catListElem = document.getElementById('cat-list');
+
+        // render this view (update the DOM elements with the right values)
+        this.render();
+    },
+
+    render: function() {
+        var cat, elem, i;
+        // get the cats we'll be rendering from the octopus
+        var cats = octopus.getCats();
+
+        // empty the cat list
+        this.catListElem.innerHTML = '';
+
+        // loop over the cats
+        for (i = 0; i < cats.length; i++) {
+            // this is the cat we're currently looping over
+            cat = cats[i];
+
+            // make a new cat list item and set its text
+            elem = document.createElement('li');
+            elem.textContent = cat.name;
+
+            // on click, setCurrentCat and render the catView
+            // (this uses our closure-in-a-loop trick to connect the value
+            //  of the cat variable to the click event function)
+            elem.addEventListener('click', (function(catCopy) {
+                return function() {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
+                };
+            })(cat));
+
+            // finally, add the element to the list
+            this.catListElem.appendChild(elem);
         }
-        view.showName();
-    },
-    eventListeners: function() {
-        const catNames = document.querySelectorAll('.cat__name');
-        catNames.forEach(catName => catName.addEventListener('click', view.showCat));
-        
-        const arrCatPics = Array.from(document.querySelectorAll('.cat__pic'));
-        arrCatPics.forEach(cat => cat.addEventListener('click', octopus.clicksCounter));
     }
-}
+};
 
-view.eventListeners();
+// make it go!
+octopus.init();
