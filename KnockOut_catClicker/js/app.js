226f8bf9659389ce07df5d2b11c2db5eacdda509
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-03-22 20:53:00 +0100

    add cats levles

--- KnockOut_catClicker/js/app.js
+++ KnockOut_catClicker/js/app.js
@@ -1,12 +1,29 @@
 var ViewModel = function() {
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
+//    this.nicknames = ko.observableArray(["Bubble", "MagicMike", "Keks"]);
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://s-media-cache-ak0.pinimg.com/originals/3f/df/b1/3fdfb172bb817793c5a842b17f714d5c.jpg')
     
     this.incrementCounter = function() {
         this.clickCount(this.clickCount() + 1);
     };
+
+    this.level = ko.computed(function() {
+        debugger;
+        if (this.clickCount() > 10) {
+            return this.level = 'Teen';
+            console.log("works");
+            
+        } else if (this.clickCount() > 5) {
+            return this.level = 'Infant';
+            console.log('infant')
+            
+        } else {
+            return this.level = "Newborn";
+            console.log('start')
+        }
+    }, this);
 }
 
 ko.applyBindings(new ViewModel());
